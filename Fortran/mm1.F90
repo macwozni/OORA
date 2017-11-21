ed27ee3620d7a3783a769abaa71a09078aa9092c
macwozni <maciekw@SkyFall.MW>
2017-11-21 10:29:43 +0100

    full multiplication

--- Fortran/mm1.F90
+++ Fortran/mm1.F90
@@ -3,13 +3,25 @@
 
 subroutine mm(first, second, multiply, ret)
    implicit none
-   real (kind = 8) :: first(SIZE, SIZE)
-   real (kind = 8) :: second(SIZE, SIZE)
+   real (kind = 8), intent(in) :: first(SIZE, SIZE)
+   real (kind = 8), intent(in) :: second(SIZE, SIZE)
    real (kind = 8) :: multiply(SIZE, SIZE)
-   integer (kind = 4) :: ret
+   integer (kind = 4), intent(out) :: ret
    integer(kind = 4) :: i, j, k;
    real (kind = 8) :: sum
-   sum = 0
+   sum = 0.d0
+
+   do i = 1, SIZE !rows in multiply
+      do j = 1, SIZE !columns in mmultiply
+         do k = 1, SIZE
+            sum = sum + first(i, k) * second(k, j)
+         end do
+         multiply(i, j) = sum
+         sum = 0.d0
+      end do
+   end do
+   ret = 0.d0
+
 end subroutine
 
 program main
@@ -19,13 +31,24 @@ program main
    real (kind = 8) :: second(SIZE, SIZE)
    real (kind = 8) :: multiply(SIZE, SIZE)
    real (kind = 8) :: dtime
+   real (kind = 8) :: mcheck
 
    do i = 1, SIZE
       do j = 1, SIZE
-         first(i, j) = i + j;
-         second(i, j) = i - j;
-         multiply(i, j) = 0.d0;
+         first(i, j) = i + j
+         second(i, j) = i - j
+      end do
+   end do
+   multiply = 0.d0 ! this is much faster than putting that in loop !!
+
+   call mm(first, second, multiply, iret)
+
+   mcheck = 0.d0;
+   do i = 1, SIZE
+      do j = 1, SIZE
+         mcheck = mcheck + multiply(i, j)
       end do
    end do
 
+   write (*, *) "check", mcheck;
 end
