59bd7829d420559908835b71d64486fc3ca672cd
macwozni <maciekw@SkyFall.MW>
2017-11-21 11:56:42 +0100

    bunch of optimizations

--- Fortran/cmm2.c
+++ Fortran/cmm2.c
@@ -19,17 +19,29 @@ double dclock() {
     return the_time;
 }
 
-/* Mulitplication of two matrices */
 int mm(double first[][SIZE], double second[][SIZE], double multiply[][SIZE]) {
-    int i, j, k;
-    double sum = 0;
-    for (i = 0; i < SIZE; i++) { //rows in multiply
-        for (j = 0; j < SIZE; j++) { //columns in multiply
-            for (k = 0; k < SIZE; k++) { //columns in first and rows in second
-                sum = sum + first[i][k] * second[k][j];
+    register unsigned int i, j, k; //<- OPT 1
+    register double sum = 0;
+    for (i = SIZE; i--;) { //rows in multiply <- OPT 3
+        for (j = SIZE; j--;) { //columns in multiply
+            for (k = 0; k < SIZE;) {
+                if (k < SIZE - 8) { //<- OPT 4
+                    sum = sum + first[i][k] * second[j][k] //<- OPT 5 (k<->j)
+                            + first[i][k + 1] * second[j][k + 1]
+                            + first[i][k + 2] * second[j][k + 2]
+                            + first[i][k + 3] * second[j][k + 3]
+                            + first[i][k + 4] * second[j][k + 4]
+                            + first[i][k + 5] * second[j][k + 5]
+                            + first[i][k + 6] * second[j][k + 6]
+                            + first[i][k + 7] * second[j][k + 7];
+                    k = k + 8;
+                } else {
+                    sum = sum + first[i][k] * second[j][k];
+                    k++;
+                }
             }
             multiply[i][j] = sum;
-            sum = 0;
+            sum = 0.0;
         }
     }
     return 0;
@@ -46,7 +58,7 @@ int main(int argc, const char* argv[]) {
     for (i = 0; i < SIZE; i++) { //rows in first
         for (j = 0; j < SIZE; j++) { //columns in first
             first[i][j] = i + j;
-            second[i][j] = i - j;
+            second[j][i] = i - j;
             multiply[i][j] = 0.0;
         }
     }
@@ -56,7 +68,7 @@ int main(int argc, const char* argv[]) {
 
     /* Here is call to matrix multiplication functionality */
     iret = mm(first, second, multiply);
-    
+
     dtime = dclock() - dtime;
     printf("Time: %le \n", dtime);
 
--- Fortran/fmm2.F90
+++ Fortran/fmm2.F90
@@ -7,7 +7,7 @@ subroutine mm(first, second, multiply, ret)
    real (kind = 8), intent(in) :: second(SIZE, SIZE)
    real (kind = 8), intent(out) :: multiply(SIZE, SIZE)
    integer (kind = 4), intent(out) :: ret
-   integer(kind = 4) :: i, j, k;
+   integer(kind = 4) :: i, j, k
    real (kind = 8) :: sum
 
    sum = 0.d0
@@ -15,8 +15,17 @@ subroutine mm(first, second, multiply, ret)
 
    do i = 1, SIZE ! columns in mmultiply
       do j = 1, SIZE ! rows in multiply
-         do k = 1, SIZE
-            sum = sum + first(i, k) * second(k, j)
+         k = 1
+         do while (k .le. SIZE)
+            sum = sum + first(i, k) * second(j, k) &
+            +first(i, k + 1) * second(j, k + 1)&
+            +first(i, k + 2) * second(j, k + 2)&
+            +first(i, k + 3) * second(j, k + 3)&
+            +first(i, k + 4) * second(j, k + 4)&
+            +first(i, k + 5) * second(j, k + 5)&
+            +first(i, k + 6) * second(j, k + 6)&
+            +first(i, k + 7) * second(j, k + 7)
+            k = k + 8
          end do
          multiply(i, j) = sum
          sum = 0.d0
@@ -39,7 +48,7 @@ program main
    do i = 1, SIZE
       do j = 1, SIZE
          first(i, j) = i + j
-         second(i, j) = i - j
+         second(j, i) = i - j
       end do
    end do
    !multiply = 0.d0 ! this is much faster than putting that in loop !!
@@ -53,12 +62,12 @@ program main
    write(*, *) 'dtime : ', dtime
 
    ! Checking part of the code. Proper value is 2.932020e+12
-   mcheck = 0.d0;
+   mcheck = 0.d0
    do i = 1, SIZE
       do j = 1, SIZE
          mcheck = mcheck + multiply(i, j)
       end do
    end do
 
-   write (*, *) "check", mcheck;
+   write (*, *) "check", mcheck
 end
