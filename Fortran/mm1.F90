1b8cdc209ca650827d5314617cc0968509369df5
macwozni <maciekw@SkyFall.MW>
2017-11-21 07:34:52 +0100

    more fortran

--- Fortran/mm1.F90
+++ Fortran/mm1.F90
@@ -1,18 +1,30 @@
 #define SIZE 512
 
 
-program main
-   integer(kind = 4) :: i, j, iret
-   real (kind = 4) :: first(SIZE, SIZE)
-   real (kind = 4) :: second(SIZE, SIZE)
-   real (kind = 4) :: multiply(SIZE, SIZE)
-   real (kind = 4) :: dtime
+subroutine mm(first, second, multiply, ret)
+   implicit none
+   real (kind = 8) :: first(SIZE, SIZE)
+   real (kind = 8) :: second(SIZE, SIZE)
+   real (kind = 8) :: multiply(SIZE, SIZE)
+   integer (kind = 4) :: ret
+   integer(kind = 4) :: i, j, k;
+   real (kind = 8) :: sum
+   sum = 0
+end subroutine
 
-   do i = 1,SIZE
-      do j = 1,SIZE
-         first(i,j) = i + j;
-         second(i,j) = i - j;
-         multiply(i,j) = 0.d0;
+program main
+   implicit none
+   integer(kind = 4) :: i, j, iret
+   real (kind = 8) :: first(SIZE, SIZE)
+   real (kind = 8) :: second(SIZE, SIZE)
+   real (kind = 8) :: multiply(SIZE, SIZE)
+   real (kind = 8) :: dtime
+
+   do i = 1, SIZE
+      do j = 1, SIZE
+         first(i, j) = i + j;
+         second(i, j) = i - j;
+         multiply(i, j) = 0.d0;
       end do
    end do
 
