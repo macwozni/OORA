31b91b6277ab97abddf1b934acf1183f767c465a
macwozni <maciekw@SkyFall.MW>
2017-11-21 15:58:51 +0100

    performance bump

--- Fortran/cmm2.c
+++ Fortran/cmm2.c
@@ -26,7 +26,7 @@ int mm(double first[][SIZE], double second[][SIZE], double multiply[][SIZE]) {
         for (j = SIZE; j--;) { //columns in multiply
             for (k = 0; k < SIZE;) {
                 if (k < SIZE - 8) { //<- OPT 4
-                    sum = sum + first[i][k] * second[j][k] //<- OPT 5 (k<->j)
+                    sum += first[i][k] * second[j][k] //<- OPT 5 (k<->j)
                             + first[i][k + 1] * second[j][k + 1]
                             + first[i][k + 2] * second[j][k + 2]
                             + first[i][k + 3] * second[j][k + 3]
@@ -36,7 +36,7 @@ int mm(double first[][SIZE], double second[][SIZE], double multiply[][SIZE]) {
                             + first[i][k + 7] * second[j][k + 7];
                     k = k + 8;
                 } else {
-                    sum = sum + first[i][k] * second[j][k];
+                    sum += first[i][k] * second[j][k];
                     k++;
                 }
             }
